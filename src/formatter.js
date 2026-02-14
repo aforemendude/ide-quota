764a52bd2f54f066e2e4c18fbc1d5dfdbdf5d5df
root <root@localhost>
2026-02-14 00:27:28 -0500

    refactor: update ANSI escape codes for dim, yellow, cyan, and white.

--- src/formatter.js
+++ src/formatter.js
@@ -1,12 +1,12 @@
 const colors = {
   reset: '\x1b[0m',
   bold: '\x1b[1m',
-  dim: '\x1b[2m',
+  dim: '\x1b[90m',
   green: '\x1b[32m',
-  yellow: '\x1b[33m',
+  yellow: '\x1b[93m',
   red: '\x1b[31m',
-  cyan: '\x1b[36m',
-  white: '\x1b[37m',
+  cyan: '\x1b[34m',
+  white: '\x1b[0m',
   magenta: '\x1b[35m',
 };
 
